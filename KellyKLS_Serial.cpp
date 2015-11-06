1328600dad5c47a27c713eb91ffb3d704987a0ec
Alex Tang <altitude@funkware.com>
2015-11-05 16:27:03 -0800

    debugging code for KellyKLS

--- KellyKLS_Serial.cpp
+++ KellyKLS_Serial.cpp
@@ -13,6 +13,7 @@ KellyKLS_Serial::KellyKLS_Serial() {
 }
 
 void KellyKLS_Serial::init ( Stream *stream2 ) {
+    //Serial.print ( "Initing KLS" );
     controllerStream = stream2;
 }
 
@@ -53,25 +54,36 @@ bool KellyKLS_Serial::readData() {
 
     if ( controllerBufferIndex == KLS8080I_LOGDATA_BUFSIZE ) {
         if ( validateChecksum() ) {
+            //Serial.println ( "Checksum good!" );
             if ( controllerBuffer[0] == REQUEST_TYPE_3A ) {
+                //Serial.println ( "Setting 3A Data" );
                 throttlePercent = ( controllerBuffer[2] - 1 ) / 255.0;
                 reverseSwitch = controllerBuffer[7];
                 batteryVoltage = controllerBuffer[11] * 1.0;
                 controllerTemp = controllerBuffer[13] * 1.0;
                 last3APacketReceivedMillis = millis();
             } else {
+                //Serial.println ( "Setting 3B Data" );
                 rpm = controllerBuffer[4] << 8 | controllerBuffer[5];
                 motorCurrent = ( controllerBuffer[6] << 8 | controllerBuffer[7] ) * 0.1;
                 last3BPacketReceivedMillis = millis();
             }
             controllerBufferIndex = 0;
+        } else {
+            //Serial.println ( "Checksum bad!" );
         }
     }
 }
 bool KellyKLS_Serial::validateChecksum() {
     uint16_t sum = 0;
     for ( int i = 0; i < KLS8080I_LOGDATA_BUFSIZE - 1; i++ ) {
+        //Serial.print ( controllerBuffer[i], HEX );
+        //Serial.print ( " " );
         sum += controllerBuffer[i];
     }
+    //Serial.print( " Packet Checksum: " );
+    //Serial.print ( controllerBuffer[18], HEX );
+    //Serial.print( " Calculated: " );
+    //Serial.println ( ( sum & 0xFF ), HEX );
     return ( ( sum & 0xFF ) == controllerBuffer[KLS8080I_LOGDATA_BUFSIZE - 1] );
 }
